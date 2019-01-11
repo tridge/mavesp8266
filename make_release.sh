d2cf883d714d2c3e1cc60b1b0d712ab432a8c94f
Buzz <davidbuzz@gmail.com>
2019-01-11 15:53:09 +1000

    bump version and test build

--- make_release.sh
+++ make_release.sh
@@ -2,12 +2,17 @@
 
 # if you want to build and rename binaries that will go to files.rfdesign.com.au, and can be uploaded through the TXMOD webpage run this:
 
+#edit these #defines in src/mavesp8266.h to match this .sh before running it. ( ie increment both if you increment either )
+#  #define MAVESP8266_VERSION_MAJOR    1
+#  #define MAVESP8266_VERSION_MINOR    3
+#  #define MAVESP8266_VERSION_BUILD    1
+
 # build firmare.bin binary:
 platformio run
 # build spiffs.bin binary:
 platformio run -t buildfs
 # copy the target binaries to a releasable name:
-VERSION=V1.30
+VERSION=V1.31
 echo cp .pioenvs/esp12e/spiffs.bin RFDTxMod-$VERSION.spiffs.bin
 cp .pioenvs/esp12e/spiffs.bin RFDTxMod-$VERSION.spiffs.bin
 echo cp .pioenvs/esp12e/firmware.bin RFDTxMod-$VERSION.bin
