0cc4079a52660037176dc4d5c897ee8531bf8e95
Matheus Ruggiero <matheusr@rfdesign.com.au>
2019-04-04 17:04:51 +1000

    BugFix on update.htm and new batch script for Windows

---
+++ make_release.bat
@@ -0,0 +1,56 @@
+@REM !/bin/bash
+
+@REM if you want to build and rename binaries that will go to files.rfdesign.com.au, and can be uploaded through the TXMOD webpage run this:
+
+@REM edit these #defines in src/mavesp8266.h to match this .sh before running it. ( ie inc@REMent both if you inc@REMent either )
+@REM  #define MAVESP8266_VERSION_MAJOR    1
+@REM  #define MAVESP8266_VERSION_MINOR    3
+@REM  #define MAVESP8266_VERSION_BUILD    1
+
+@REM ------------------------------------------------------------------
+
+@REM set us up for a 2MB build:
+copy platformio.ini.2m platformio.ini
+
+del .pioenvs\esp12e\firmware.bin
+del .pioenvs\esp12e\spiffs.bin
+del firmware.bin
+del spiffs.bin
+
+@REM build firmare.bin binary:
+platformio run
+@REM build spiffs.bin binary:
+platformio run -t buildfs
+
+@REM copy the target binaries to a releasable name:
+@SET VERSION=V1.32
+copy .pioenvs\esp12e\spiffs.bin .
+rename spiffs.bin RFDTxMod-%VERSION%.2m.spiffs.bin
+copy .pioenvs\esp12e\firmware.bin .
+rename firmware.bin RFDTxMod-%VERSION%.2m.bin
+
+del .pioenvs\esp12e\firmware.bin
+del .pioenvs\esp12e\spiffs.bin
+del firmware.bin
+del spiffs.bin
+
+@REM------------------------------------------------------------------
+
+@REM set us up for a 4MB build:
+copy platformio.ini.4m platformio.ini
+
+@REM build firmare.bin binary:
+platformio run
+@REM build spiffs.bin binary:
+platformio run -t buildfs
+
+@REM copy the target binaries to a releasable name:
+copy .pioenvs\esp12e\spiffs.bin .
+rename spiffs.bin RFDTxMod-%VERSION%.4m.spiffs.bin
+copy .pioenvs\esp12e\firmware.bin .
+rename firmware.bin RFDTxMod-%VERSION%.4m.bin
+
+@REM------------------------------------------------------------------
+echo ------------------------------------------------------------------
+@REM show user:
+@dir RFDTx*.bin
